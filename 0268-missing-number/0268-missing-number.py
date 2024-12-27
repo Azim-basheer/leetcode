90bc8cd5285416d4b43b9ecc1471bcd96aeef7d1
Azim-basheer <azimohamed15@gmail.com>
2024-12-27 08:12:02 +0530

    Time: 0 ms (100.00%), Space: 18.9 MB (6.64%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        diff=sum(range(len(nums)+1))-sum(nums)
-        return diff
+        return sum(range(len(nums)+1))-sum(nums)
+         
             
